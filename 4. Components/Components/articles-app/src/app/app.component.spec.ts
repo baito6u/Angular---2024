548a3ef92df2261f38438438afc6fd2e6ea1c284
Todor Krumov <31984842+baito6u@users.noreply.github.com>
2024-02-23 06:55:50 +0200

    Angular Components Lab - Articles
    until Problem 4.3 Implement the Read More Function

--- 4. Components/Components/articles-app/src/app/app.component.spec.ts
+++ 4. Components/Components/articles-app/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-ang16-app'`, () => {
+  it(`should have as title 'articles-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-ang16-app');
+    expect(app.title).toEqual('articles-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-ang16-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('articles-app app is running!');
   });
 });
---
+++ 4. Components/Components/articles-app/src/app/article/article.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+import { Input } from "@angular/core";
+import { Article } from "../models/article.model";
+
+
+@Component({
+    selector: 'app-article',
+    templateUrl: './article.component.html',
+    styleUrls: ['./article.component.css'],
+})
+
+export class ArticleComponent {
+    private symbols: number = 250;
+    @Input() article: Article;
+    @Input() articleDesc: string;
+    descToShow: string;
+    articleDescLen: number;
+    showReadMoreBtn: boolean = false;
+    imageIsShown: boolean = false;
+    imageButtonTitle: string = "Show Image";
+
+    constructor() {
+        this.articleDescLen = 0;
+        this.descToShow = "";
+    }
+}
