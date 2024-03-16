cc9f8712fa16b5bc203eca794b19e1eb22c06c6c
Todor Krumov <31984842+baito6u@users.noreply.github.com>
2024-03-16 07:47:03 +0200

    register form
    register foum started validations

--- 10. Workshop Forms/Worksop Forms/workshop-forms/src/app/user/register/register.component.ts
+++ 10. Workshop Forms/Worksop Forms/workshop-forms/src/app/user/register/register.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { EMAIL_DOMAINS } from 'src/app/constants';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', Validators.required, emailValidator(EMAIL_DOMAINS)],
+    tel: [''],
+    passGroup: this.fb.group(
+      {
+        password: ['', [Validators.required]],
+        rePassword: ['', [Validators.required]],
+      }
+    )
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  register():void {
+    if(this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+    
+  }
+}
